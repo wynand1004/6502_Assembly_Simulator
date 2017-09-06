70fe0a3b4faee75f1d72e1b84c726b252402f1db
Christian Thompson <wynand1004@gmail.com>
2017-09-06 18:26:44 +0900

    Version 0.1.2
    Added Text Display 40 x 5 Characters

--- simlulator.py
+++ simlulator.py
@@ -6,29 +6,45 @@ root = tkinter.Tk()
 root.title("6502 Assembler Simulator")
 
 class Display(object):
-	def __init__(self):
+	def __init__(self, start_of_video_memory, start_of_character_memory):
 		self.canvas = tkinter.Canvas(root, width=320, height=240)
 		self.canvas.pack()
+
+		self.label = tkinter.Label(root, width=40, height=5, bg="black", fg="white")
+		self.label.pack()		
 		
-		self.start_of_video_memory = 1024
-		self.start_of_characater_memory = 2048
+		self.start_of_video_memory = start_of_video_memory
+		self.start_of_character_memory = start_of_character_memory
 		self.colors = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white"]
 		
 	def update(self):
+
+		# Update Graphics Canvas
 		# Clear canvas
 		self.canvas.delete("all") 	
 	
-		# Draw 32 x 24 Squares (10 x 10)
+		# Draw 32 x 24 Squares (10 x 10 pixels)
 		for x in range(0, 32):
 			for y in range(0, 24):
 				m = self.start_of_video_memory + (y * 32) + (x)
 				color = self.colors[memory[m]]
 				self.canvas.create_rectangle(x * 10, y * 10, x * 10 + 10, y * 10 + 10, fill=color)	
+
+		# Update text label (40 x 5 Characters)
+		text = ""
+		for i in range(self.start_of_character_memory, self.start_of_character_memory + 200):
+			text += chr(memory[i])
+			# Check for eol
+			if (i - (self.start_of_character_memory - 1)) % 40 == 0:
+				text += "\r"
 		
+		# Update label
+		self.label["text"]=text
+
 		# Update
 		root.update()
 
-display = Display()
+display = Display(1024, 2048)
 
 # Memory
 memory = []
@@ -37,8 +53,12 @@ for _ in range(0, 65536):
 
 
 while True:
-	for i in range(1024, 1024 + 768):
+	for i in range(1024 + 768):
 		memory[i] = random.randint(0, 7)
+
+	for i in range(2048, 2248):
+		memory[i] = random.randint(65, 90)
+		
 		
 	display.update()
 	
