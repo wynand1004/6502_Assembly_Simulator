ef3e2d572acad46fdff031d5e344106b1625fecd
Christian Thompson <wynand1004@gmail.com>
2017-09-06 13:05:52 +0900

    Version 0.1.1 (Memory to Video Mapping)
    Memory to Video Mapping Correction

--- simlulator.py
+++ simlulator.py
@@ -1,11 +1,9 @@
 #Testing for Simulated Computer
-
 import tkinter
 import random
 
 root = tkinter.Tk()
 root.title("6502 Assembler Simulator")
-
 
 class Display(object):
 	def __init__(self):
@@ -16,7 +14,6 @@ class Display(object):
 		self.start_of_characater_memory = 2048
 		self.colors = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white"]
 		
-		
 	def update(self):
 		# Clear canvas
 		self.canvas.delete("all") 	
@@ -24,24 +21,26 @@ class Display(object):
 		# Draw 32 x 24 Squares (10 x 10)
 		for x in range(0, 32):
 			for y in range(0, 24):
-				m = self.start_of_video_memory + (y * 24) + (x * 32)
+				m = self.start_of_video_memory + (y * 32) + (x)
 				color = self.colors[memory[m]]
 				self.canvas.create_rectangle(x * 10, y * 10, x * 10 + 10, y * 10 + 10, fill=color)	
 		
 		# Update
 		root.update()
 
-		# Wait 30 ms and do it again
-		root.after(30, self.update)
-
 display = Display()
 
 # Memory
 memory = []
-for _ in range(0, 65535):
-	memory.append(random.randint(0, 7))
+for _ in range(0, 65536):
+	memory.append(0)
 
-#root.after(50, draw_random_colors)
-root.after(50, display.update)
+
+while True:
+	for i in range(1024, 1024 + 768):
+		memory[i] = random.randint(0, 7)
+		
+	display.update()
+	
 
 root.mainloop()
