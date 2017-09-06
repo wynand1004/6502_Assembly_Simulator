6a2497605c4607e33850b806bc108dc298bcb640
Christian Thompson <wynand1004@gmail.com>
2017-09-06 12:23:07 +0900

    Version 0.1 Display Class

--- simlulator.py
+++ simlulator.py
@@ -6,27 +6,42 @@ import random
 root = tkinter.Tk()
 root.title("6502 Assembler Simulator")
 
-screen = tkinter.Canvas(root, width=320, height=240)
-screen.pack()
 
-colors = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white"]
+class Display(object):
+	def __init__(self):
+		self.canvas = tkinter.Canvas(root, width=320, height=240)
+		self.canvas.pack()
+		
+		self.start_of_video_memory = 1024
+		self.start_of_characater_memory = 2048
+		self.colors = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white"]
+		
+		
+	def update(self):
+		# Clear canvas
+		self.canvas.delete("all") 	
+	
+		# Draw 32 x 24 Squares (10 x 10)
+		for x in range(0, 32):
+			for y in range(0, 24):
+				m = self.start_of_video_memory + (y * 24) + (x * 32)
+				color = self.colors[memory[m]]
+				self.canvas.create_rectangle(x * 10, y * 10, x * 10 + 10, y * 10 + 10, fill=color)	
+		
+		# Update
+		root.update()
 
-def draw_random_colors():
-    # Clear canvas
-    screen.delete("all") 
+		# Wait 30 ms and do it again
+		root.after(30, self.update)
 
-    # Draw 32 x 24 Squares (10 x 10) with random colors
-    for x in range(0, 32):
-        for y in range(0, 24):
-            color = random.choice(colors)
-            screen.create_rectangle(x * 10, y * 10, x * 10 + 10, y * 10 + 10, fill=color)    
+display = Display()
 
-    # Show changes
-    root.update()
+# Memory
+memory = []
+for _ in range(0, 65535):
+	memory.append(random.randint(0, 7))
 
-    # Wait 30 ms and do it again
-    root.after(30, draw_random_colors)
-
-root.after(50, draw_random_colors)
+#root.after(50, draw_random_colors)
+root.after(50, display.update)
 
 root.mainloop()
