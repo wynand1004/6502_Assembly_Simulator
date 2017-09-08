326ce020246c98036d63dd8f6437aca57281b5de
Christian Thompson <wynand1004@gmail.com>
2017-09-08 15:17:19 +0900

    CPU Update (Registers, Flags, Program Counter)

--- 6502_cpu.py
+++ 6502_cpu.py
@@ -1,27 +1,61 @@
 class CPU(object):
-	def __init__(self):
-		pass
+	def __init__(self, pc=0x1000):
 		# Create registers
-		
+		self.a = 0
+		self.x = 0
+		self.y = 0
 		
 		# Create flags
-		
+		self.carry = False
+		self.decimal = False
+		self.interrupt = False
+		self.negative = False
+		self.overflow = False
+		self.zero = False
 		
 		# Create program counter
-		
+		self.pc = pc
 		
 	def tick(self, memory):
-		pass
 		
-# For testing purposes only
-import os
-os.system("clear")
+		#INX
+		if value == 0xe8:
+			self.x += 0x01
+			if self.x > 0xff:
+				self.x = 0x00
+				self.carry_flag = True
+			self.pc += 0x01
+		
+		#INY
+		if value == 0xc8:
+			self.y += 0x01
+			if self.y > 0xff:
+				self.y = 0x00
+				self.carry_flag = True		
+			self.pc += 0x01
 
-print("6502 CPU Simulator")
-print("Registers:")
-print("")
-print("Flags:")
-print("")
-print("Program Counter: ")
-print("")
+
+if __name__ == "__main__":	
+	# For testing purposes only
+	import os
+	os.system("clear")
+
+	cpu = CPU(0x1000)
+
+	print("6502 CPU Simulator")
+	print("Registers:")
+	print("A: {}".format(cpu.a))
+	print("X: {}".format(cpu.x))
+	print("Y: {}".format(cpu.y))
+	print("")
+	print("Flags:")
+	print("Carry: {}".format(cpu.carry))
+	print("Decimal: {}".format(cpu.decimal))
+	print("Interrupt Disable: {}".format(cpu.interrupt))
+	print("Negative: {}".format(cpu.negative))
+	print("Overflow: {}".format(cpu.overflow))
+	print("Zero: {}".format(cpu.zero))
+	print("")
+	print("Program Counter: ")
+	print("Program Counter: {}".format(cpu.pc))
 
