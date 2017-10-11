367189bda7416774d1d4597f86f63cd6866a2951
Christian Thompson <wynand1004@gmail.com>
2017-10-11 17:38:56 +0900

    Updated parsing / Verify basic addressing modes

--- asm_to_opcode.py
+++ asm_to_opcode.py
@@ -2,9 +2,9 @@ asm = """
 LDA #$01
 STA $400
 INC $1003
-TAX 
-INX 
-TXA 
+TAX
+INX
+TXA
 CMP #$0f
 BEQ $1012
 JMP $1002
@@ -15,16 +15,42 @@ JMP $1002
 
 import re
 
-tokens = re.split(' |\n', asm)
+lines = re.split('\n', asm)
 
-for token in tokens:
-	token = token.strip()
-	if token == "TAX":
-		print("0xe8")
-	elif token == "TXA":
-		print("0x1a")
-	elif len(token) == 0:
-		pass
+for line in lines:
+	mode = ""
+	tokens = re.split(' ', line.strip())
+
+	if len(tokens) == 1:
+		# Deal with single length opcodes first
+		token = tokens[0].strip()
+
+		if token == "TAX":
+			print("0xe8")
+		elif token == "TXA":
+			print("0x1a")
+
+	elif len(tokens) == 2:
+		
+		# Decide addressing mode
+		value = tokens[1].strip()
+
+		# Check to see if the first character is a # or $
+		if value[0] in "$0123456789":
+			mode = "direct"
+			if value[0] == "$":
+				value = value[1:]
+				
+		elif value[0] == "#":
+			mode = "immediate"
+			if value[1] == "$":
+				value = value[2:]
+			else:
+				value = value[1:]	
+
+		# Temp output
+		print(mode)
+		print (value)	
 	else:
 		print(token)
 	
